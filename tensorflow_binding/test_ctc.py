c5cedb7a7bf6cf5bde15bba2f0a06bb691cb4d06
Rewon Child <rewonc@gmail.com>
2016-10-10 12:25:10 -0700

    Disable inactive GPU tests

--- tensorflow_binding/test_ctc.py
+++ tensorflow_binding/test_ctc.py
@@ -10,7 +10,6 @@ class CTCLossTest(tf.test.TestCase):
                      alphabet_size, expected_loss,
                      expected_gradients, expected_error=None):
         self.assertEquals(data.shape, expected_gradients.shape)
-
         data_t = tf.constant(data)
         data_lengths_t = tf.constant(data_lengths)
         flat_labels_t = tf.constant(flat_labels)
@@ -116,85 +115,36 @@ class CTCLossTest(tf.test.TestCase):
     #                       expected_loss=expected_loss,
     #                       expected_gradients=expected_gradients)
 
-    def test_larger_cpu(self):
-        """Test two batch entries (From Tensorflow)"""
-        # max_time_steps == 7
-        depth = 6
-        # seq_len_0 == 5
-        targets_0 = [0, 1, 2, 1, 0]
-        loss_log_prob_0 = -3.34211
-        # dimensions are time x depth
-        input_prob_matrix_0 = np.asarray(
-            [[0.633766, 0.221185, 0.0917319, 0.0129757, 0.0142857, 0.0260553],
-             [0.111121, 0.588392, 0.278779, 0.0055756, 0.00569609, 0.010436],
-             [0.0357786, 0.633813, 0.321418, 0.00249248, 0.00272882, 0.0037688],
-             [0.0663296, 0.643849, 0.280111, 0.00283995, 0.0035545, 0.00331533],
-             [0.458235, 0.396634, 0.123377, 0.00648837, 0.00903441, 0.00623107]],
-            dtype=np.float32)
-        input_log_prob_matrix_0 = np.log(input_prob_matrix_0)
-        gradient_log_prob_0 = np.asarray(
-            [[-0.366234, 0.221185, 0.0917319, 0.0129757, 0.0142857, 0.0260553],
-             [0.111121, -0.411608, 0.278779, 0.0055756, 0.00569609, 0.010436],
-             [0.0357786, 0.633813, -0.678582, 0.00249248, 0.00272882, 0.0037688],
-             [0.0663296, -0.356151, 0.280111, 0.00283995, 0.0035545, 0.00331533],
-             [-0.541765, 0.396634, 0.123377, 0.00648837, 0.00903441, 0.00623107]],
-            dtype=np.float32)
+    def test_multiple_batches(self):
+        activations = np.array([
+            [0.1, 0.6, 0.1, 0.1, 0.1],
+            [0.1, 0.1, 0.6, 0.1, 0.1]
+        ], dtype=np.float32)
 
-        # seq_len_1 == 5
-        targets_1 = [0, 1, 1, 0]
-        loss_log_prob_1 = -5.42262
-        # dimensions are time x depth
+        alphabet_size = 5
+        # dimensions should be t, n, p: (t timesteps, n minibatches,
+        # p prob of each alphabet). This is one instance, so expand
+        # dimensions in the middle
+        _data = np.expand_dims(activations, 1)
+        data = np.concatenate([_data, _data[...]], axis=1)
+        labels = np.asarray([1, 2, 1, 2], dtype=np.int32)
+        expected_loss = np.asarray([2.46286, 2.46286], dtype=np.float32)
+        gradients = np.asarray([
+            [0.177031, -0.708125, 0.177031, 0.177031, 0.177031],
+            [0.177031, 0.177031, -0.708125, 0.177031, 0.177031]
+        ], dtype=np.float32)
+        _expected_gradients = np.expand_dims(gradients, 1)
+        expected_gradients = np.concatenate(
+            [_expected_gradients, _expected_gradients[...]], axis=1)
 
-        input_prob_matrix_1 = np.asarray(
-            [[0.30176, 0.28562, 0.0831517, 0.0862751, 0.0816851, 0.161508],
-             [0.24082, 0.397533, 0.0557226, 0.0546814, 0.0557528, 0.19549],
-             [0.230246, 0.450868, 0.0389607, 0.038309, 0.0391602, 0.202456],
-             [0.280884, 0.429522, 0.0326593, 0.0339046, 0.0326856, 0.190345],
-             [0.423286, 0.315517, 0.0338439, 0.0393744, 0.0339315, 0.154046]],
-            dtype=np.float32)
-        input_log_prob_matrix_1 = np.log(input_prob_matrix_1)
-        gradient_log_prob_1 = np.asarray(
-            [[-0.69824, 0.28562, 0.0831517, 0.0862751, 0.0816851, 0.161508],
-             [0.24082, -0.602467, 0.0557226, 0.0546814, 0.0557528, 0.19549],
-             [0.230246, 0.450868, 0.0389607, 0.038309, 0.0391602, -0.797544],
-             [0.280884, -0.570478, 0.0326593, 0.0339046, 0.0326856, 0.190345],
-             [-0.576714, 0.315517, 0.0338439, 0.0393744, 0.0339315, 0.154046]],
-            dtype=np.float32)
+        label_lengths = np.asarray([2, 2], dtype=np.int32)
+        data_lengths = np.asarray([2, 2], dtype=np.int32)
 
-        # len max_time_steps array of 2 x depth matrices
-        inputs = [np.vstack([input_log_prob_matrix_0[t, :],
-                             input_log_prob_matrix_1[t, :]])
-                  for t in range(5)] + 2 * [np.nan * np.ones((2, depth),
-                                            np.float32)]
-
-        # convert inputs into [max_time x batch_size x depth tensor] Tensor
-        inputs = np.asarray(inputs)
-
-        # len batch_size array of label vectors
-        labels = np.asarray(targets_0 + targets_1, dtype=np.int32)
-        label_lengths = np.asarray([len(targets_0), len(targets_1)],
-                                   dtype=np.int32)
-
-        # batch_size length vector of sequence_lengths
-        seq_lens = np.asarray([5, 5], dtype=np.int32)
-
-        # output: batch_size length vector of negative log probabilities
-        loss_truth = np.array([-loss_log_prob_0, -loss_log_prob_1], np.float32)
-
-        # output: len max_time_steps array of 2 x depth matrices
-        grad_truth = [np.vstack([gradient_log_prob_0[t, :],
-                                 gradient_log_prob_1[t, :]])
-                      for t in range(5)] + 2 * [np.zeros((2, depth),
-                                                np.float32)]
-
-        # convert grad_truth into [max_time x batch_size x depth] Tensor
-        grad_truth = np.asarray(grad_truth)
-
-        self._run_ctc_cpu(inputs, data_lengths=seq_lens,
+        self._run_ctc_cpu(data, data_lengths=data_lengths,
                           flat_labels=labels, label_lengths=label_lengths,
-                          alphabet_size=depth,
-                          expected_loss=loss_truth,
-                          expected_gradients=grad_truth)
+                          alphabet_size=alphabet_size,
+                          expected_loss=expected_loss,
+                          expected_gradients=expected_gradients)
 
 
 if __name__ == "__main__":
--- tensorflow_binding/warpctc/warp.cc
+++ tensorflow_binding/warpctc/warp.cc
@@ -3,20 +3,21 @@
 #include "tensorflow/core/framework/allocator.h"
 #include "./ctc.h"
 #include <iostream>
+// #include <csignal>
 
 
-// forward declare CUstream to avoid pulling in cuda headers
-typedef struct CUstream_st* CUstream;
+// // forward declare CUstream to avoid pulling in cuda headers
+// typedef struct CUstream_st* CUstream;
 
-// forward declare stream headers to avoid pulling in stream_executor code
-namespace perftools {
-namespace gputools {
-class Stream;
-namespace cuda {
-  CUstream AsCUDAStreamValue(Stream *stream);
-}  // namespace cuda
-}  // namespace gputools
-}  // namespace perftools
+// // forward declare stream headers to avoid pulling in stream_executor code
+// namespace perftools {
+// namespace gputools {
+// class Stream;
+// namespace cuda {
+//   CUstream AsCUDAStreamValue(Stream *stream);
+// }  // namespace cuda
+// }  // namespace gputools
+// }  // namespace perftools
 
 
 REGISTER_OP("WarpCTC")
@@ -73,7 +74,7 @@ class WarpCTCOpCPU : public OpKernel {
     auto grads = gradients->flat<float>();
 
     // compute CTC
-    float costs;
+    std::vector<float> costs(n_minibatches);
     ctcStatus_t stat_compute = compute_ctc_loss(data.data(),
                                                 grads.data(),
                                                 labels.data(),
@@ -81,9 +82,10 @@ class WarpCTCOpCPU : public OpKernel {
                                                 data_lens.data(),
                                                 alphabet_size,
                                                 n_minibatches,
-                                                &costs,
+                                                costs.data(),
                                                 scratch,
                                                 info);
+    // std::raise(SIGINT);
 
     a->DeallocateRaw(scratch);
 
@@ -91,93 +93,94 @@ class WarpCTCOpCPU : public OpKernel {
                 errors::Internal("Error in CTC computation"))
 
     Tensor* loss_t = nullptr;
-    OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape({1}), &loss_t));
+    OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape({n_minibatches}), &loss_t));
     auto loss = loss_t->flat<float>();
-    loss(0) = costs;
-
+    for (int i = 0; i < n_minibatches; ++i) {
+      loss(i) = costs[i];
+    }
   }
  private:
   int alphabet_size_;
 };
 
-#define EIGEN_USE_GPU
+// #define EIGEN_USE_GPU
 
-class WarpCTCOpGPU : public OpKernel {
- public:
-  explicit WarpCTCOpGPU(OpKernelConstruction* context) : OpKernel(context) {
-    OP_REQUIRES_OK(context, context->GetAttr("alphabet_size", &alphabet_size_));
-  }
+// class WarpCTCOpGPU : public OpKernel {
+//  public:
+//   explicit WarpCTCOpGPU(OpKernelConstruction* context) : OpKernel(context) {
+//     OP_REQUIRES_OK(context, context->GetAttr("alphabet_size", &alphabet_size_));
+//   }
 
-  void Compute(OpKernelContext* context) override {
+//   void Compute(OpKernelContext* context) override {
 
-    const Tensor& data_t = context->input(0);
-    const Tensor& data_lens_t = context->input(1);
-    const Tensor& labels_t = context->input(2);
-    const Tensor& label_lens_t = context->input(3);
-    auto data = data_t.flat<float>();
-    auto data_lens = data_lens_t.flat<int32>();
-    auto labels = labels_t.flat<int32>();
-    auto label_lens = label_lens_t.flat<int32>();
-    int alphabet_size = alphabet_size_;
-    int n_minibatches = data_t.dim_size(1);
+//     const Tensor& data_t = context->input(0);
+//     const Tensor& data_lens_t = context->input(1);
+//     const Tensor& labels_t = context->input(2);
+//     const Tensor& label_lens_t = context->input(3);
+//     auto data = data_t.flat<float>();
+//     auto data_lens = data_lens_t.flat<int32>();
+//     auto labels = labels_t.flat<int32>();
+//     auto label_lens = label_lens_t.flat<int32>();
+//     int alphabet_size = alphabet_size_;
+//     int n_minibatches = data_t.dim_size(1);
 
-    // for debugging -- these values appear to not be set. Why?
-    // the values for some reason do not appear to live on the GPU
-    // maybe it is using the cpu eigen, or something?
-    // float n0 = data(0);
-    // int n1 = data_lens(0);
-    // int n2 = labels(0);
-    // int n3 = label_lens(0);
+//     // for debugging -- these values appear to not be set. Why?
+//     // the values for some reason do not appear to live on the GPU
+//     // maybe it is using the cpu eigen, or something?
+//     // float n0 = data(0);
+//     // int n1 = data_lens(0);
+//     // int n2 = labels(0);
+//     // int n3 = label_lens(0);
 
-    // ctcComputeInfo info;
-    // info.loc = CTC_GPU;
-    // info.stream = perftools::gputools::cuda::AsCUDAStreamValue(
-    //   context->device()->tensorflow_gpu_device_info()->stream);
-    // size_t gpu_alloc_size;
-    // ctcStatus_t stat_alloc = get_workspace_size(label_lens.data(), data_lens.data(),
-    //                                             alphabet_size, n_minibatches, info,
-    //                                             &gpu_alloc_size);
-    // OP_REQUIRES(context, (stat_alloc == CTC_STATUS_SUCCESS),
-    //             errors::Internal("Error in CTC memory estimation"))
+//     // ctcComputeInfo info;
+//     // info.loc = CTC_GPU;
+//     // info.stream = perftools::gputools::cuda::AsCUDAStreamValue(
+//     //   context->device()->tensorflow_gpu_device_info()->stream);
+//     // size_t gpu_alloc_size;
+//     // ctcStatus_t stat_alloc = get_workspace_size(label_lens.data(), data_lens.data(),
+//     //                                             alphabet_size, n_minibatches, info,
+//     //                                             &gpu_alloc_size);
+//     // OP_REQUIRES(context, (stat_alloc == CTC_STATUS_SUCCESS),
+//     //             errors::Internal("Error in CTC memory estimation"))
 
-    // // // allocate scratch space for ctc computation
-    // Allocator* a = context->device()->GetAllocator(AllocatorAttributes());
-    // void* scratch = a->AllocateRaw(1, gpu_alloc_size);
+//     // // // allocate scratch space for ctc computation
+//     // Allocator* a = context->device()->GetAllocator(AllocatorAttributes());
+//     // void* scratch = a->AllocateRaw(1, gpu_alloc_size);
 
-    // // // allocate gradient tensor
-    // Tensor* gradients = NULL;
-    // OP_REQUIRES_OK(context, context->allocate_output(1, data_t.shape(),
-    //                                                  &gradients));
-    // auto grads = gradients->flat<float>();
+//     // // // allocate gradient tensor
+//     // Tensor* gradients = NULL;
+//     // OP_REQUIRES_OK(context, context->allocate_output(1, data_t.shape(),
+//     //                                                  &gradients));
+//     // auto grads = gradients->flat<float>();
 
-    // // // compute CTC
-    // float costs;
-    // ctcStatus_t stat_compute = compute_ctc_loss(data.data(),
-    //                                             grads.data(),
-    //                                             labels.data(),
-    //                                             label_lens.data(),
-    //                                             data_lens.data(),
-    //                                             alphabet_size,
-    //                                             n_minibatches,
-    //                                             &costs,
-    //                                             scratch,
-    //                                             info);
+//     // // // compute CTC
+//     // float costs;
+//     // ctcStatus_t stat_compute = compute_ctc_loss(data.data(),
+//     //                                             grads.data(),
+//     //                                             labels.data(),
+//     //                                             label_lens.data(),
+//     //                                             data_lens.data(),
+//     //                                             alphabet_size,
+//     //                                             n_minibatches,
+//     //                                             &costs,
+//     //                                             scratch,
+//     //                                             info);
 
-    // a->DeallocateRaw(scratch);
+//     // a->DeallocateRaw(scratch);
 
-    // OP_REQUIRES(context, (stat_compute == CTC_STATUS_SUCCESS),
-    //             errors::Internal("Error in CTC computation"))
+//     // OP_REQUIRES(context, (stat_compute == CTC_STATUS_SUCCESS),
+//     //             errors::Internal("Error in CTC computation"))
 
-    // Tensor* loss_t = nullptr;
-    // OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape({1}), &loss_t));
-    // auto loss = loss_t->flat<float>();
-    // loss(0) = 0.5;
-  }
- private:
-  int alphabet_size_;
-};
+//     // Tensor* loss_t = nullptr;
+//     // OP_REQUIRES_OK(context, context->allocate_output(0, TensorShape({1}), &loss_t));
+//     // auto loss = loss_t->flat<float>();
+//     // loss(0) = 0.5;
+//   }
+//  private:
+//   int alphabet_size_;
+// };
 
-#undef EIGEN_USE_GPU
+// #undef EIGEN_USE_GPU
 
 REGISTER_KERNEL_BUILDER(Name("WarpCTC").Device(DEVICE_CPU), WarpCTCOpCPU);
-REGISTER_KERNEL_BUILDER(Name("WarpCTC").Device(DEVICE_GPU), WarpCTCOpGPU);
+// REGISTER_KERNEL_BUILDER(Name("WarpCTC").Device(DEVICE_GPU), WarpCTCOpGPU);
